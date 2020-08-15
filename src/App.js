6ffe56d3cb3e71a62c161bc187f081ea3601fd54
ICLUSTERS <iclusters@Apples-MacBook-Pro-2.local>
2020-08-15 17:03:25 +0530

    updates happens every second

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { blue } from '@material-ui/core/colors';
+import AWS from "aws-sdk";
+import { Consumer } from 'sqs-consumer';
+
+
+
 
 const useStyles = theme => ({
   table: {
@@ -18,38 +23,104 @@ const useStyles = theme => ({
   },
 });
 
-
-class App extends Component {
+class App extends React.Component {
   intervalID;
 
-  state = {
-    data: [],
-  }
+  constructor(props) {
+    super(props)
+    this.state = {
+        data: []
+    }
+
+    this.getData = this.getData.bind(this); 
+
+}
+   componentDidMount() {
+
+    const script = document.createElement("script");
+    script.src = "https://sdk.amazonaws.com/js/aws-sdk-2.734.0.min.js";
+    script.async = true;
+  
+    document.body.appendChild(script);
+
+    AWS.config.region = 'ap-south-1'; // Region
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'ap-south-1:9edbe760-688b-41f6-8715-5bcb57fe8357',
+    });
 
 
-
-  componentDidMount() {
     document.body.style.backgroundColor = "black"
-    this.getData();
+     this.firstpopulate();
+     this.getData();
+     
   }
 
   componentWillUnmount() {
     clearTimeout(this.intervalID);
   }
 
-  getData = () => {
+  getData() {
+
+    var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+    var queueURL = "https://sqs.ap-south-1.amazonaws.com/563469033471/forexqueue";
+    var params = {
+      AttributeNames: [
+         "All"
+      ],
+      MaxNumberOfMessages: 10,
+      MessageAttributeNames: [
+         "All"
+      ],
+      QueueUrl: queueURL,
+      VisibilityTimeout: 20,
+      WaitTimeSeconds: 0
+     };
+
+    
+    sqs.receiveMessage(params, (err, data)=> {
+       if (err) {
+        console.log("Receive Error", err);
+
+      } else if (data.Messages) {
+          data.Messages.forEach((item) =>{
+            let single = JSON.parse(item.Body).Message;
+            let Curre =  single.slice(13,19)
+            let Timestampe =  single.slice(44,59)
+            let Ratee = single.slice(-11).slice(0 , -1).replace("\"","").replace(" ", "");
+            const stateCopy = [...this.state.data];
+
+            console.log(stateCopy);
+            console.log(Curre, Timestampe)
+            stateCopy.forEach((element , i) => {
+              if(element.Curr == Curre){
+                stateCopy[i] = {...stateCopy[i] , Timestamp : Timestampe}        
+                stateCopy[i] = {...stateCopy[i] , Rate : Ratee}                    
+            
+              }});
+              this.setState({data : [...stateCopy]});
+
+
+          })
+
+      }
+    });
+    this.intervalID = setTimeout(this.getData.bind(this), 1000);
+
+
+}
+
+  firstpopulate =() =>{
     fetch('https://lktkd58l71.execute-api.ap-south-1.amazonaws.com/staging/forex')
-      .then(response => response.json())
-      .then(data => {
-          data.forEach((element, i )=> {
-            element.Curr = element.Curr.slice(0,3) + "/" + element.Curr.slice(3, 6) 
+    .then(response => response.json())
+    .then(data => {
+        data.forEach((element, i )=> {
+          if(element.Curr == "“ABC/DEF”"){
+            data.splice(i , 1)
+          }
           });
-        this.setState({ data: [...data] });
-        this.intervalID = setTimeout(this.getData.bind(this), 30000);
-      });
-
-
-    }
+      this.setState({ data: [...data] });
+    });
+  }
   render() {
     const { classes } = this.props;
     return (
@@ -58,7 +129,7 @@ class App extends Component {
 
  
       <img src={logo} className="App-logo" alt="logo" />
-      <div className = "headerTable"> Forex Table (Table updates after every min)</div>
+      <div className = "headerTable"> Forex Table (Table updates after every second)</div>
 
 
       <TableContainer component={Paper}>
